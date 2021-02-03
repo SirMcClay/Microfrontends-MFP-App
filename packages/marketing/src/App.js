1f74814caf06c09c1ea9b2e394468217aee9ccd9
sirmcclay <mcclay.design@gmail.com>
2021-02-03 12:27:41 -0300

    marketing: take history as a App prop

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -12,11 +12,11 @@ const generateClassName = createGenerateClassName({
 	productionPrefix: 'ma',
 });
 
-export default () => {
+export default ({ history }) => {
 	return (
 		<div>
 			<StylesProvider generateClassName={generateClassName}>
-				<Router>
+				<Router history={history}>
 					<Switch>
 						<Route exact path="/pricing" component={Pricing} />
 						<Route path="/" component={Landing} />
