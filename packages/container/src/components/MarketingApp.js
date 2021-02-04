706879e7e35d6409bea72573dc427b6b88dc5b12
sirmcclay <mcclay.design@gmail.com>
2021-02-04 09:53:20 -0300

    container: now using useHistory to update the path in browser governed from container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,15 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
 	const ref = useRef(null);
+	const history = useHistory();
 
 	useEffect(() => {
 		mount(ref.current, {
 			onNavigate: ({ pathname: nextPathname }) => {
-				console.log(nextPathname);
+				history.push(nextPathname);
 			},
 		});
 	});
